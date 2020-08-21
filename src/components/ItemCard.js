822c1efca4fbe5655dbd2353e9d3535dcffd6e85
Craxus <estebanjlemes1@gmail.com>
2020-08-21 01:40:28 -0300

    Nuevos estados en items
    Mostrar descripcion al hacer hover y agregar al carrito desde carrito generado al hacer hover

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Mother from '../assets/images/motherboard.jpg';
 
 const ItemCard = (props) => {
 
+    const [hiddeDesc, setHiddeDesc] = useState('hidden');
+
+    const show = () => setHiddeDesc('hidden');
+    const hidde = () => setHiddeDesc('');
+
     return(
         <div>
             <div className="card" style={{width: 300}}>
-                <a href="" onClick={(e) => {
-                    props.setCart(props.cart+1)
-                    e.preventDefault();
-                }}>
+                <a onMouseLeave={show} onMouseMove={hidde} class="item" href="">
                     <img className="card-img-top" src={Mother} alt="Cards"/>
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div className={"card-body", hiddeDesc}>
+                        <p className="card-text p-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <i 
+                            onClick={(e) => {
+                                props.setCart(props.cart+1)
+                                e.preventDefault();
+                            }} 
+                            className="cart-in-item fas fa-shopping-cart"
+                        />
                     </div>
                 </a>
             </div>
